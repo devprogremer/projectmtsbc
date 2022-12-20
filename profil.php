859b672f56c7987c4c56d5da05205029e8184052
devprogremer <devprogremer@gmail.com>
2022-12-20 16:55:04 +0700

    rev2

--- profil.php
+++ profil.php
@@ -99,21 +99,21 @@
 <section id="content"><div class="ic">MTS Bina Cendekia 2022</div>
 <div class="camera-wrapper">
     <div id="camera" class="camera-wrap">
-        <div data-src="images/index_slide01.jpg">
+        <div data-src="images/index_slide1.jpg">
             <div class="fadeIn camera_caption">
                 <!--<h2 class="text_1 color_1">Solutions that you need!</h2>-->
                 <a class="btn_1" href="#">Daftar</a>
             </div>
         </div>
-        <div data-src="images/index_slide02.jpg">
+        <div data-src="images/index_slide2.jpeg">
             <div class="fadeIn camera_caption">
-                <h2 class="text_1 color_1">Lorem ipsum dolor sit!</h2>
+                <h2 class="text_1 color_1">Selamat Datang di MTS Bina Cendekia Cirebon</h2>
                 <a class="btn_1" href="#">Daftar</a>
             </div>
         </div>
-        <div data-src="images/index_slide03.jpg">
+        <div data-src="images/index_slide3.jpg">
             <div class="fadeIn camera_caption">
-                <h2 class="text_1 color_1">Lorem ipsum dolor sit amet, consectetur!</h2>
+                <h2 class="text_1 color_1">Sekolah Kita Gudangnya Akhlakul Karimah</h2>
                 <a class="btn_1" href="#">More info</a>
             </div>
         </div>
