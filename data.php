079950ef34942e62d00e08cb4c8df4a652faf84e
devprogremer <devprogremer@gmail.com>
2022-12-22 15:30:19 +0700

    rev1

--- data.php
+++ data.php
@@ -1,1 +1,162 @@
-data guru
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>MTS Bina Cendekia</title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no"/>
+    <link rel="icon" href="images/favicon-32x32.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/camera.css"/>
+    <link rel="stylesheet" href="css/owl.carousel.css"/>
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery-migrate-1.2.1.js"></script>
+    <script src="js/jquery.equalheights.js"></script>
+    <!--[if (gt IE 9)|!(IE)]><!-->
+    <script src="js/jquery.mobile.customized.min.js"></script>
+    <!--<![endif]-->
+    <script src="js/camera.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <!--[if lt IE 9]>
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+            <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0"
+                 height="42" width="820"
+                 alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+        </a>
+    </div>
+    <script src="js/html5shiv.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
+    <![endif]-->
+</head>
+<body>
+<div class="page">
+<!--========================================================
+                          HEADER
+=========================================================-->
+<header id="header">
+    <div id="stuck_container">
+        <div class="container">
+            <div class="row">
+                <div class="grid_12">
+                    <div class="brand put-left">
+                        <h1>
+                            <a href="index.html">
+                                <img src="images/logomts.png" alt="Logo"/>
+                            </a>
+                        </h1>
+                    </div>
+                    <nav class="nav put-right">
+                        <ul class="sf-menu">
+                            <li class="current"><a href="index.php">Home</a></li>
+                            <li>
+                                <a href="#">Tentang Kami</a>
+                                <ul>
+                                    <li><a href="profil.php">Profil Sekolah</a></li>
+                                    <li><a href="visi.php">Visi Dan Misi</a>
+                                    <li><a href="organisasi.php">Struktur Organisasi</a>
+                                    <li><a href="data.php">Data Guru</a>
+                                    <li><a href="akreditasi.php">Akreditasi</a>
+                                    <li><a href="sarana">Sarana</a>
+                                     <ul>
+                                            <li><a href="#">Ruang Kelas</a></li>
+                                            
+                                        </ul>
+
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Elearning</a></li>
+                            <li>
+                                <a href="#">Pendaftaran</a>
+                                <ul>
+                                    <li><a href="#">Brosur</a></li>
+                                    <li><a href="#">Pendaftaran</a>
+                            </ul>
+                            <li>
+                                <a href="#">Galeri Kegiatan</a>
+                                <ul>
+                                    <li><a href="#">Kegiatan Sekolah</a></li>
+                                    <li><a href="#">Ekstrakulikuler</a>
+                                                                           
+                                        </ul>
+
+                                    </li>
+                                    <li>
+                                <a href="http://rdm.mtsbinacendekia.sch.id/">Rapot Digital</a>
+</li>
+</ul>
+                           
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!--========================================================
+                          CONTENT
+=========================================================-->
+<section id="content"><div class="ic">MTS Bina Cendekia 2022</div>
+<div class="camera-wrapper">
+    <div id="camera" class="camera-wrap">
+        <div data-src="images/index_slide01.jpg">
+            <div class="fadeIn camera_caption">
+                <!--<h2 class="text_1 color_1">Solutions that you need!</h2>-->
+                <a class="btn_1" href="#">Daftar</a>
+            </div>
+        </div>
+        <div data-src="images/index_slide02.jpg">
+            <div class="fadeIn camera_caption">
+                <h2 class="text_1 color_1">Sekolah Kita Gudangnya Akhlakul Karimah</h2>
+                <a class="btn_1" href="#">Daftar</a>
+            </div>
+        </div>
+        <div data-src="images/index_slide03.jpg">
+            <div class="fadeIn camera_caption">
+                <h2 class="text_1 color_1">Selamat Datang Di Mts Bina Cendekia Cirebon</h2>
+                <a class="btn_1" href="#">More info</a>
+            </div>
+        </div>
+    </div>
+</div>
+<h2 class="header_1 wrap_8 color_2 " style="text-align:left">Daftar Data Guru </h2>
+<p class="wrap_8" style=text-align:"left">Berikut Daftar Data Guru Mts Bina Cendekia </p>
+<center><img src="images/DataGuru.png" width="1000" height="700"><center>
+
+<div class="container">
+    <div class="row wrap_9 wrap_4 wrap_10">
+        <div class="grid_12">
+            <div class="header_1 wrap_3 color_3">
+                Get in Touch
+            </div>
+            <div class="box_3">
+                <ul class="list_1">
+                    <li><a class="fa fa-twitter" href="#"></a></li>
+                    <li><a class="fa fa-facebook" href="#"></a></li>
+                    <li><a class="fa fa-google-plus" href="#"></a></li>
+                    <li><a class="fa fa-pinterest" href="#"></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+</div>
+<!--========================================================
+                          FOOTER
+=========================================================-->
+<footer id="footer" class="color_9">
+    <div class="container">
+        <div class="row">
+            <div class="grid_12">
+                <p class="info text_4 color_4">
+                    © <span id="copyright-year"></span> | <a href="#">Privacy Policy</a> <br/>
+                    Website designed by <a href="http://www.templatemonster.com/" rel="nofollow">TemplateMonster.com</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="js/script.js"></script>
+</body>
+</html>
