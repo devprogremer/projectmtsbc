ea2d43b536fe2214de56299dd38a9c39bfca8e4a
devprogremer <devprogremer@gmail.com>
2022-12-22 14:41:25 +0700

    rev1

--- data.php
+++ data.php
@@ -99,19 +99,19 @@
 <section id="content"><div class="ic">MTS Bina Cendekia 2022</div>
 <div class="camera-wrapper">
     <div id="camera" class="camera-wrap">
-        <div data-src="images/index_slide01.jpg">
+        <div data-src="images/index_slide1.jpg">
             <div class="fadeIn camera_caption">
                 <!--<h2 class="text_1 color_1">Solutions that you need!</h2>-->
                 <a class="btn_1" href="#">Daftar</a>
             </div>
         </div>
-        <div data-src="images/index_slide02.jpg">
+        <div data-src="images/index_slide2.jpeg">
             <div class="fadeIn camera_caption">
                 <h2 class="text_1 color_1">Sekolah Kita Gudangnya Akhlakul Karimah</h2>
                 <a class="btn_1" href="#">Daftar</a>
             </div>
         </div>
-        <div data-src="images/index_slide03.jpg">
+        <div data-src="images/index_slide3.jpg">
             <div class="fadeIn camera_caption">
                 <h2 class="text_1 color_1">Selamat Datang Di Mts Bina Cendekia Cirebon</h2>
                 <a class="btn_1" href="#">More info</a>
@@ -119,128 +119,9 @@
         </div>
     </div>
 </div>
-<div class="container">
-    <div class="row wrap_1 wrap_5">
-        <div class="grid_3">
-            <div class="box_1">
-                <div class="icon_1"></div>
-                <h3 class="text_2 color_2 maxheight1"><a href="#">Sambutan Kepala Sekolah</a></h3>
-                <p class="text_3 color_2 maxheight">
-                    Lorem ipsum dolor sit amettetur dipiscing elit. In mollis erat mattis
-                </p>
-                <a class="btn_2" href="#">read more</a></div>
-        </div>
-        <div class="grid_3">
-            <div class="box_1">
-                <div class="icon_2"></div>
-                <h3 class="text_2 color_2 maxheight1"><a href="#">Marketing Research</a></h3>
-                <p class="text_3 color_2 maxheight">
-                    Dorem ipsum dolor sit amettetur dipiscing elit. In mollis erat mattis
-                </p>
-                <a class="btn_2" href="#">read more</a></div>
-        </div>
-        <div class="grid_3">
-            <div class="box_1">
-                <div class="icon_3"></div>
-                <h3 class="text_2 color_2 maxheight1"><a href="#">Employee Benefits</a></h3>
-                <p class="text_3 color_2 maxheight">
-                    Kerem ipsum dolor sit amettetur dipiscing elit. In mollis erat mattis
-                </p>
-                <a class="btn_2" href="#">read more</a></div>
-        </div>
-        <div class="grid_3">
-            <div class="box_1">
-                <div class="icon_4"></div>
-                <h3 class="text_2 color_2 maxheight1"><a href="#">Risk Management</a></h3>
-                <p class="text_3 color_2 maxheight">
-                    Norem ipsum dolor sit amettetur dipiscing elit. In mollis erat mattis neque facilisis, sit
-                    amet uesas
-                </p>
-                <a class="btn_2" href="#">read more</a></div>
-        </div>
-    </div>
-</div>
-<div class="bg_1 wrap_2 wrap_4">
-    <div class="container">
-        <div class="row">
-            <div class="preffix_2 grid_8">
-                <h2 class="header_2 wrap_3 color_3">
-                    <center>Mts Bina Cendekia Cirebon<center></br>
-                        <center> Sekolah Kita Gudangnya Akhlakul Karimah <center>
-                </h2>
-            </div>
-        </div>
-        
-<div class="container">
-    <div class="row wrap_1">
-        <div class="grid_6">
-            <div class="wrap_6">
-                <div class="box_2 maxheight2">
-            
-                    <div class="caption">
-                        <h3 class="text_2 color_2">
-                            Visi Mts Bina Cendekia Cirebon <br/>
-                        </h3>
-                        <p class="text_3">
-                        Madrasah Tsanawiyah (MTs) Bina Cendekia mempunyai visi menjadi OUTSTANDING ENTREPRENEURIAL ISLAMIC JUNIOR HIGH SCHOOL. Satuan pendidikan ini menyelenggarakan pendidikan Islami yang unggul, pada jenjang pendidikan dasar, guna membangun sumber daya insani Muslim yang berjiwa pewirausaha.
-                    </p>
-                     <a href="#" class="btn_2">read more</a>
-                    </div>
-                    <div class="clearfix"></div>
-                </div>
-            </div>
-        </div>
-        <div class="grid_6">
-            <div class="wrap_6">
-                <div class="box_2 maxheight2">
-                   
-                        <div class="caption">
-                            <h3 class="text_2 color_2">
-                                Misi Mts Bina Cendekia Cirebon <br/>
-            
-                            </h3>
-                            <p class="text_3">
-                               1. Memberi kontribusi kepada program pemerintah Indonesia terkait penyediaan lembaga pendidikan yang memberi kesempatan kepada anak usia MTs/SMP, khususnya Muslim, untuk memperoleh pendidikan dasar berkualitas. </br>
-                               2. Memberi kontribusi kepada program pemerintah Indonesia terkait pelaksanaan wajib belajar sembilan tahun.
-                               3. Memberi layanan kepada anak-anak Indonesia, khususnya Muslim, dalam memperoleh pengetahuan tentang Agama Islam beserta nilai-nilai dan prakatek amalannya dalam kehidupan sehari-hari.<br/>
-                               4. Memberi layanan kepada anak-anak Indonesia, khususnya Muslim, untuk mempersiapkan diri melanjutkan pendidikan pada jenjang pendidikan menengah. <br/>
-                               5. Memberi layanan kepada anak-anak Indonesia, khususnya Muslim, untuk mempersiapkan diri melanjutkan pendidikan pada jenjang pendidikan menengah. <br/>
-                            </p>
-                            <a href="#" class="btn_2">read more</a>
-                        </div>
-                        <div class="clearfix"></div>
-                    </div>
-                </div>
-           
-        </div>
-    </div>
-</div>
-<div class="bg_1 wrap_7 wrap_5">
-    <div class="container">
-        <div class="row">
-            <div class="grid_12">
-                <center><h2 class="header_2 wrap_8 color_3"><center>
-                    Tujuan Mts Bina Cendekia Cirebon
-                </h2>
-            </div>
-        </div>
-        <div class="row">
-            <div class="grid_12">
-                <div id="owl">
-                    <div class="item">
-                        <p class="text_3">
-                        Memiliki pengetauan dan kompetensi yang memadai untuk melanjutkan pendidikan ke jenjang pendidikan menengah atau memasuki kehidupan di masyrakat.
-Menguasai konten kurikulum MTs, baik terkait pengetahuan, keterampilan, maupun sikap sehingga menjadi warga negara terdidik.
-Memiliki keimanan yang kuat serta memiliki pengetahuan tentang Islam, terutama terkait dengan aturan-aturan dalam melaksanakan amalan-amalan ibadah wajib dan bukan wajib.
-Mampu membaca Al Qur’an secara fasih dan lancar serta mengerti maknanya.
-Memiliki kecakapan dan motivasi berwirausaha sehingga mandiri dan mampu memecahkan masalah yang dihadapi serta dapat berkontribusi secara positif kepada masyarakatnya.<br/>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+        <h2 class="header_1 wrap_7 color_2 fs-3" style="text-align:left">◆ Berikut Adalah Daftar Data Guru</h2>
+        <p class="fs-5" style="text-align:left"> Berikut Adalah Daftar Data Guru Mts Bina Cendekia Cirebon </p>
+<center><img src="images/DataGuru.png" width="1000" height="700"><center>
 <div class="container">
     <div class="row wrap_9 wrap_4 wrap_10">
         <div class="grid_12">
