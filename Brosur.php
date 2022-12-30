d406e38a7d29dd1c56a22034b52c29afd20a6faf
devprogremer <devprogremer@gmail.com>
2022-12-30 17:24:33 +0700

    rev

--- Brosur.php
+++ Brosur.php
@@ -1,1 +1,306 @@
-Brosur
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>MTS Bina Cendekia</title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no"/>
+    <link rel="icon" href="images/favicon-32x32.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/camera.css"/>
+    <link rel="stylesheet" href="css/owl.carousel.css"/>
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery-migrate-1.2.1.js"></script>
+    <script src="js/jquery.equalheights.js"></script>
+    <!--[if (gt IE 9)|!(IE)]><!-->
+    <script src="js/jquery.mobile.customized.min.js"></script>
+    <!--<![endif]-->
+    <script src="js/camera.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <!--[if lt IE 9]>
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+            <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0"
+                 height="42" width="820"
+                 alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+        </a>
+    </div>
+    <script src="js/html5shiv.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
+    <![endif]-->
+</head>
+<body>
+<div class="page">
+<!--========================================================
+                          HEADER
+=========================================================-->
+<header id="header">
+    <div id="stuck_container">
+        <div class="container">
+            <div class="row">
+                <div class="grid_12">
+                    <div class="brand put-left">
+                        <h1>
+                            <a href="index.html">
+                                <img src="images/logomts.png" alt="Logo"/>
+                            </a>
+                        </h1>
+                    </div>
+                    <nav class="nav put-right">
+                        <ul class="sf-menu">
+                            <li class="current"><a href="index.php">home</a></li>
+                            <li>
+                                <a href="#">Tentang Kami</a>
+                                <ul>
+                                    <li><a href="profil.php">Profil Sekolah</a></li>
+                                    <li><a href="visi.php">Visi Dan Misi</a>
+                                    <li><a href="organisasi.php">Struktur Organisasi</a>
+                                    <li><a href="data.php">Data Guru</a>
+                                    <li><a href="akreditasi.php">Akreditasi</a>
+                                    <li><a href="sarana.php">Sarana</a>
+                                     <ul>
+                                            <li><a href="#">Ruang Kelas</a></li>
+                                            
+                                        </ul>
+
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Elearning</a></li>
+                            <li>
+                                <a href="#">Pendaftaran</a>
+                                <ul>
+                                    <li><a href="brosur.php">Brosur</a></li>
+                                    <li><a href="pendaftaran.php">Pendaftaran</a>
+                            </ul>
+                            <li>
+                                <a href="#">Galeri Kegiatan</a>
+                                <ul>
+                                    <li><a href="kegiatan.php">Kegiatan Sekolah</a></li>
+                                    <li><a href="ekstrakulikuler.php">Ekstrakulikuler</a>
+                                                                           
+                                        </ul>
+
+                                    </li>
+                                    <li>
+                                <a href="http://rdm.mtsbinacendekia.sch.id/">Rapot Digital</a>
+</li>
+</ul>
+                           
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!--========================================================
+                          CONTENT
+=========================================================-->
+<section id="content"><div class="ic">MTS Bina Cendekia 2022</div>
+<div class="camera-wrapper">
+    <div id="camera" class="camera-wrap">
+        <div data-src="images/index_slide1.jpg">
+            <div class="fadeIn camera_caption">
+                <!--<h2 class="text_1 color_1">Solutions that you need!</h2>-->
+                <a class="btn_1" href="#">Daftar</a>
+            </div>
+        </div>
+        <div data-src="images/index_slide2.jpeg">
+            <div class="fadeIn camera_caption">
+                <h2 class="text_1 color_1">Selamat Datang di MTS Bina Cendekia Cirebon</h2>
+                <a class="btn_1" href="#">Daftar</a>
+            </div>
+        </div>
+        <div data-src="images/index_slide3.jpg">
+            <div class="fadeIn camera_caption">
+                <h2 class="text_1 color_1">Sekolah Kita Gudangnya Akhlakul Karimah</h2>
+                <a class="btn_1" href="#">More info</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row wrap_1 wrap_5">
+        <div class="grid_3">
+            <div class="box_1">
+                <div class="icon_1"></div>
+                <h3 class="text_2 color_2 maxheight1"><a href="#">Sambutan Kepala Sekolah</a></h3>
+                <p class="text_3 color_2 maxheight">
+                    Lorem ipsum dolor sit amettetur dipiscing elit. In mollis erat mattis
+                </p>
+                <a class="btn_2" href="#">read more</a></div>
+        </div>
+        <div class="grid_3">
+            <div class="box_1">
+                <div class="icon_2"></div>
+                <h3 class="text_2 color_2 maxheight1"><a href="#">Marketing Research</a></h3>
+                <p class="text_3 color_2 maxheight">
+                    Dorem ipsum dolor sit amettetur dipiscing elit. In mollis erat mattis
+                </p>
+                <a class="btn_2" href="#">read more</a></div>
+        </div>
+        <div class="grid_3">
+            <div class="box_1">
+                <div class="icon_3"></div>
+                <h3 class="text_2 color_2 maxheight1"><a href="#">Employee Benefits</a></h3>
+                <p class="text_3 color_2 maxheight">
+                    Kerem ipsum dolor sit amettetur dipiscing elit. In mollis erat mattis
+                </p>
+                <a class="btn_2" href="#">read more</a></div>
+        </div>
+        <div class="grid_3">
+            <div class="box_1">
+                <div class="icon_4"></div>
+                <h3 class="text_2 color_2 maxheight1"><a href="#">Risk Management</a></h3>
+                <p class="text_3 color_2 maxheight">
+                    Norem ipsum dolor sit amettetur dipiscing elit. In mollis erat mattis neque facilisis, sit
+                    amet uesas
+                </p>
+                <a class="btn_2" href="#">read more</a></div>
+        </div>
+    </div>
+</div>
+<div class="bg_1 wrap_2 wrap_4">
+    <div class="container">
+        <div class="row">
+            <div class="preffix_2 grid_8">
+            <div class="header_1 wrap_3 color_2">
+             Sejarah Perkembangan Madrasah Tsanawiyah Bina Cendekia 
+            </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="grid_12">
+                <div class="box_4">
+                    <p class="text_">
+                    Sejarah berdirinya Yayasan Bina Cendekia Utama Cirebon (YBCUC) dimulai dari direalisaikannya komitmen Prof. Dr, Mohammad Ali, MA, untuk memberi tunjangan setiap bulan kepada para guru Al Qur’an dan para guru pengajian kitab agama Islam klasik, serta merehabilitasi musholla, masjid jami’ dan membeli sarana dan prasarana madarsah sesuai kebutuhan, mulai 10 Maret, 2002. 
+                    Beliau yang sejak tahun 1980 mejadi Dosen, dan pada tahun 2001 dikukuhkan sebagai Profesor, di Universitas Pendidikan Indonesia (UPI), Bandung adalah salah seorang putra dari Almaghfur lah K.H. M. Sya’roni, guru pengajian kitab agama Islam yang pada tahun 1985-1990 menjadi Kepala Madrasah Aliyah PATWA. Pada tahun 1980, Ali  mendirikan yayasan YABISA yang menyelenggarakan SMA YABISA, namun pada tahun 1992 yayasan itu merger dan menjadi yayasan PATWA.
+                    Komitmen beliau itu sesungguhnya merupakan kepedulian terhadap pendidikan, khususnya keberlangsungan penyelenggaraan pendidikan dan da’wah Islamiyah. Oleh karena itu, agar komitmen tersebut berkelanjutan, pada tahun 2008 beliau mendirikan sebuah yayasan baru yang dikukuhkan dengan Akta Pendirian Yayasan Nomor 03, Tanggal 1 September, 2008. Yayasan beserta Anggaran Dasarnya disahkan oleh Kementerian Hukum dan Hak Asasi Manusia (Kemenkumham) dengan SK Menkumham RI Nomor AHU-324.AH.01.04, Tahun 2009. Dewasa ini yayasan baru itu telah membuka 6 (enam) satuan pendidikan, baik pada jenjang pendidikan dasar, pendidikan menengah, maupun pendidikan tinggi. Empat satuan pendidikan menyelenggarakan pendidikan yang berada pada jurisdiksi Kementerian Agama (Kemenag) dan tiga satuan pendidikan menyelenggarakan pendidikan dalam jurisdiksi Kementerian Pendidikan dan Kebudayaan (Kemdikbud). Saat ini yayasan sedang mengajukan proposal pendirian satuan pendidikan ketujuh kepada Kemdikbud, RI, yaitu Institut Sains dan Teknologi Terapan (ISTT).
+                    Salah satu instansi lembaga y6ang ada didalamnya adalah MTs Bina Cendekia. Adapun lokasinya masih dalam komplek Yayasan Bina Cendekia Utama Cirebon. MTs Bina Cendekia berdiri pada tahun 2010. Adapun para kepala Madrasah yang pernah menjabat diantaranya:</br>
+                    1. H. Hasanuddin, S.Ag </br>
+                    2. H. Djidinuddin </br>
+                    3. Drs. H. Wahuri </br>
+                    4. Mohammad Qushoy, M.Pd </br>
+                    Sebagai konsekuensi Lembaga yang dikelola oleh Dapartemen Agama tidak sama nasib dan perjuangannya dengan Lembaga pendidikannya yang dikelola oleh dinas pada umumnya mayoritas masyartakat purwakarta pada umumnya lebih memilih SMP (Sekolah Menengah Pertama) untuk Menyekolahkan anaknya ketimbang masuk MTs, apalgai siswa yang mempunyai kompetensi dasar lebih tinggi, sehingga pada umumnya orang tua yang berkemampuan ekonomi menengah kebawahlah yang mau menyekolahkan putra-putrinya ke sekolah MTs, maka sudah barang tentu input siswa yang diterima di MTs tidak sebaik yang diterima di MTs.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row wrap_1">
+        <div class="grid_6">
+            <div class="wrap_6">
+                <div class="box_2 maxheight2">
+            
+                    <div class="caption">
+                        <h3 class="text_2 color_2">
+                            Identitas Satuan Pendidikan <br/>
+                        </h3>
+                        <p class="text_3">
+                                Nama	:	MTSS BINA CENDEKIA<br/>
+                            	NPSN	:	20278791<br/>
+                             	Alamat	:	Jl. Kh. Wahid Hasyim Mertapadawetan<br/>
+ 	                    Desa/Kelurahan	:	Mertapada Wetan<br/>
+                 	Kecamatan/Kota (LN)	:	KEC. ASTANA JAPURA<br/>
+               	Kab.-Kota/Negara (LN)	:	KAB. CIREBON<br/>
+ 	          Propinsi/Luar Negeri (LN)	:	PROV. JAWA BARAT<br/>
+                    	Status Sekolah	:	SWASTA<br/>
+ 	               Bentuk Pendidikan	:	MTs<br/>
+                    </p>
+                     <a href="#" class="btn_2">read more</a>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+        <div class="grid_6">
+                <div class="wrap_6">
+                    <div class="box_2 maxheight2">
+                        <div class="put-left"><img src="images/index_img2.png" alt="Image 2"/></div>
+                        <div class="caption">
+                            <h3 class="text_2 color_2">
+                                Lokasi<br/>
+            
+                            </h3>
+                            <p class="text_3">
+                            5JPG+G92,Mertapada Wetan,Kec.Astanajapura,Kabupaten Cirebon,Jawa Barat 45181
+                            </p>
+                            <a href="#" class="btn_2">read more</a>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+           
+        </div>
+    </div>
+</div>
+<div class="bg_1 wrap_7 wrap_5">
+    <div class="container">
+        <div class="row">
+            <div class="grid_12">
+                <h2 class="header_1 wrap_8 color_4">
+                    General Information
+                </h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="grid_12">
+                <div id="owl">
+                    <div class="item">
+                        <p class="text_3">
+                     Madrasah Tsanawiyah (MTs) Bina Cendekia adalah satuan pendidikan yang berada dalam jurisdiksi Kementerian Agama (Kemenag). Menurut UU Nomor 20 Tahun 2003, tentang Sistem Pendidikan Nasional, satuan pendidikan ini menjadi terminal jenjang pendidikan dasar yang setara dengan Sekolah Menengah Pertama (SMP) yang berada pada jurisdiksi Kementerian Pendidikan dan Kebudayaan (Kemdikbud). Sebagai bagian dari sistem pendidikan nasional, satuan pendidikan ini dapat menjadi alternatif bagi lulusan Sekolah Dasar (SD) atau Madrasah Ibtidaiyah (MI) untuk melanjutkan pendidikan dalam rangka melaksanakan wajib belajar 9 tahun. Satu satunya perbedaan antara pendidikan di MTs dan di SMP adalah jumlah jam pelajaran yang dialokasikan untuk Pendidikan Agama Islam (PAI) yang pada kurikulum MTs dialokasikan 6 jam pelajaran, 
+                    sedangkan pada kurikulum SMP hanya 2 jam pelajaran.      
+                            <br/>
+                        </p>
+                    </div>
+                    <div class="item">
+                        <p class="text_3">
+                     Pelaksanaan pendidikan di MTs Bina Cendekia mengintegrasikan PAI ke dalam program pendidikan Pesantren. Target yang ingin dicapai adalah setiap peserta didik mampu membaca Al Qur’an secara fasih dan lancar serta memahami dasar-dasar fiqih ibadah dan mu’amalah Islami, dan mampu melaksanakan prakteknya. Dalam pelaksanaannya,
+                     peserta didik dapat mencapai target itu dengan mengikuti kegiatan komplementer di Pesantren. Melalui cara ini diharapkan setiap peserta didik memperoleh kompetensi terkait dengan mata pelajaran PAI melebihi yang ditargetkan untuk dicapai melalui pelaksanaan kurikulum PAI di SMP.       
+                            <br/>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row wrap_9 wrap_4 wrap_10">
+        <div class="grid_12">
+            <div class="header_1 wrap_3 color_3">
+                Get in Touch
+            </div>
+            <div class="box_3">
+                <ul class="list_1">
+                    <li><a class="fa fa-twitter" href="#"></a></li>
+                    <li><a class="fa fa-facebook" href="#"></a></li>
+                    <li><a class="fa fa-google-plus" href="#"></a></li>
+                    <li><a class="fa fa-pinterest" href="#"></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+</div>
+<!--========================================================
+                          FOOTER
+=========================================================-->
+<footer id="footer" class="color_9">
+    <div class="container">
+        <div class="row">
+            <div class="grid_12">
+                <p class="info text_4 color_4">
+                    © <span id="copyright-year"></span> | <a href="#">Privacy Policy</a> <br/>
+                    Website designed by <a href="http://www.templatemonster.com/" rel="nofollow">TemplateMonster.com</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="js/script.js"></script>
+</body>
+</html>
