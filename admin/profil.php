9317180e86b6a77b2a3df6b1eab5e2ab324da5ad
“devprogremer” <devprogremer@gmai.com>
2023-01-14 01:53:27 +0700

    14 01

--- admin/profil.php
+++ admin/profil.php
@@ -45,7 +45,7 @@
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
         <img src="assets/img/logo.png" alt="">
-        <span class="d-none d-lg-block">NiceAdmin</span>
+        <span class="d-none d-lg-block">Admin</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
@@ -274,7 +274,7 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="admin/home.php">
+        <a class="nav-link collapsed" href="home.php">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
@@ -286,7 +286,7 @@
         </a>
         <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
-            <a href="Profil/profil.php">
+            <a href="profil.php">
               <i class="bi bi-circle"></i><span>Profil Sekolah</span>
             </a>
           </li>
@@ -359,8 +359,58 @@
     <section class="section">
       <div class="row">
         <div class="col-lg-6">
+        <center><a href="tambah_produk.php">+ &nbsp; Tambah Produk</a><center>
+    <br/>
+    <table>
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Produk</th>
+          <th>Dekripsi</th>
+          <th>Harga Beli</th>
+          <th>Harga Jual</th>
+          <th>Gambar</th>
+          <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+      <?php
+      // jalankan query untuk menampilkan semua data diurutkan berdasarkan nim
+      $query = "SELECT * FROM produk ORDER BY id ASC";
+      $result = mysqli_query($kon, $query);
+      //mengecek apakah ada error ketika menjalankan query
+      if(!$result){
+        die ("Query Error: ".mysqli_errno($kon).
+           " - ".mysqli_error($kon));
+      }
 
+      //buat perulangan untuk element tabel dari data mahasiswa
+      $no = 1; //variabel untuk membuat nomor urut
+      // hasil query akan disimpan dalam variabel $data dalam bentuk array
+      // kemudian dicetak dengan perulangan while
+      while($row = mysqli_fetch_assoc($result))
+      {
+      ?>
+       <tr>
+          <td><?php echo $no; ?></td>
+          <td><?php echo $row['nama_produk']; ?></td>
+          <td><?php echo substr($row['deskripsi'], 0, 20); ?>...</td>
+          <td>Rp <?php echo number_format($row['harga_beli'],0,',','.'); ?></td>
+          <td>Rp <?php echo $row['harga_jual']; ?></td>
+          <td style="text-align: center;"><img src="gambar/<?php echo $row['gambar_produk']; ?>" style="width: 120px;"></td>
+          <td>
+              <a href="edit_produk.php?id=<?php echo $row['id']; ?>">Edit</a> |
+              <a href="proses_hapus.php?id=<?php echo $row['id']; ?>" onclick="return confirm('Anda yakin akan menghapus data ini?')">Hapus</a>
+          </td>
+      </tr>
          
+      <?php
+        $no++; //untuk nomor urut terus bertambah 1
+      }
+      ?>
+    </tbody>
+    </table>
+
 
         <div class="col-lg-6">
 
